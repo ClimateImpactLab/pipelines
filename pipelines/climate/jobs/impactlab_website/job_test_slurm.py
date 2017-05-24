b3133f88262230774112363a018b94782201703f
Michael Delgado <mdelgado@rhg.com>
2017-05-24 13:36:57 -0700

    add job test:

---
+++ pipelines/climate/jobs/impactlab_website/job_test_slurm.py
@@ -0,0 +1,89 @@
+'''
+Example BCSD transformation pipeline definition file
+'''
+
+
+from __future__ import absolute_import
+import os
+
+import pipelines
+import pipelines.climate.transformations as trn
+
+from pipelines.climate.toolbox import (
+    load_climate_data,
+    weighted_aggregate_grid_to_regions,
+    bcsd_transform)
+
+
+__author__ = 'Michael Delgado'
+__contact__ = 'mdelgado@rhg.com'
+__version__ = '0.1.0'
+
+BCSD_orig_files = os.path.join(
+    '/shares/gcp/sources/BCSD-original/{rcp}/day/atmos/{variable}/r1i1p1/v1.0',
+    '{variable}_day_BCSD_{rcp}_r1i1p1_{model}_{year}.nc')
+
+WRITE_PATH = os.path.join(
+    '/shares/gcp/outputs/diagnostics/web/test/climate/{agglev}/{scenario}',
+    '{variable}/{variable}_{model}_{period}.nc')
+
+ADDITIONAL_METADATA = dict(
+    description=__doc__.strip(),
+    author=__author__,
+    contact=__contact__,
+    version=__version__,
+    repo='https://github.com/ClimateImpactLab/pipelines',
+    file='/climate/jobs/impactlab-website/bcsd-orig.py',
+    execute='climate.jobs.impactlab_website.bcsd_orig.main',
+    project='gcp', 
+    team='climate',
+    geography='hierid',
+    weighting='areawt',
+    frequency='20yr')
+
+JOBS = [
+    dict(variable='tas', transformation=trn.average_seasonal_temp)]
+
+PERIODS = [
+    dict(rcp='historical', pername='1986', years=list(range(1986, 2006))),
+    dict(rcp='rcp85', pername='2020', years=list(range(2020, 2040))),
+    dict(rcp='rcp85', pername='2040', years=list(range(2040, 2060))),
+    dict(rcp='rcp85', pername='2080', years=list(range(2080, 2100)))]
+
+MODELS = list(map(lambda x: dict(model=x), [
+    'ACCESS1-0',
+    'bcc-csm1-1',
+    'BNU-ESM',
+    'CanESM2',
+    'CCSM4',
+    'CESM1-BGC',
+    'CNRM-CM5',
+    'CSIRO-Mk3-6-0',
+    'GFDL-CM3',
+    'GFDL-ESM2G',
+    'GFDL-ESM2M',
+    'IPSL-CM5A-LR',
+    'IPSL-CM5A-MR',
+    'MIROC-ESM-CHEM',
+    'MIROC-ESM',
+    'MIROC5',
+    'MPI-ESM-LR',
+    'MPI-ESM-MR',
+    'MRI-CGCM3',
+    'inmcm4',
+    'NorESM1-M']))
+
+AGGREGATIONS = [{'agglev': 'grid025', 'aggwt': 'unweighted'}]
+
+
+@pipelines.register('web_bcsd_climate_data_template')
+@pipelines.add_metadata(ADDITIONAL_METADATA)
+@pipelines.read_pattern(BCSD_orig_files)
+@pipelines.write_pattern(WRITE_PATH)
+@pipelines.iterate(JOBS, PERIODS, MODELS, AGGREGATIONS)
+@pipelines.run(workers=1)
+def web_bcsd_climate_data_template(*args, **kwargs):
+    bcsd_transform(*args, **kwargs)
+
+if __name__ == '__main__':
+    web_bcsd_climate_data_template().run()
