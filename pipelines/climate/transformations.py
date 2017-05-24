ebcb2c0956923dadbb10c15a73a6694d79fc8ece
Michael Delgado <mdelgado@rhg.com>
2017-05-23 22:44:18 -0700

    clean up tests

--- pipelines/climate/transformations.py
+++ pipelines/climate/transformations.py
@@ -24,28 +24,3 @@ def average_seasonal_temp(ds):
     Average seasonal tas
     '''
     return ds.tas.groupby('time.season').mean(dim='time')
-
-
-# @pipelines.document
-# def example_transformation_annual_binned_tas(ds):
-#     '''
-#     Example computation: annual binned tas
-    
-#     See also
-#     --------
-#     xarray.DataArray.groupby_bins
-#     pandas.cut
-
-#     '''
-
-#     bins_tas_C = [0, 10, 14, 16, 19, 23, 31, 40]
-
-
-#     return (ds
-#         .groupby_bins(
-#               'tas',  # name of the variable to be grouped
-#               bins_tas_C,  # bin definitions
-#               right=True,  # right-inclusive, i.e. (0, 10], (10, 14], ...
-#               include_lowest=True  # first interval should be left-inclusive
-#               )
-#         .count(dim='time'))  # count number of days in each bin for the year
