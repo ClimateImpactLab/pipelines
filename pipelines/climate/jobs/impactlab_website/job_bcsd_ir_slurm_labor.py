f3a08ed6fb18d68007c846a6879dae122ed9ab68
jgerardsimcock <justin_simcock@yahoo.com>
2017-05-30 14:26:31 -0700

    working on sac, now test on brc

--- pipelines/climate/jobs/impactlab_website/job_bcsd_ir_slurm_labor.py
+++ pipelines/climate/jobs/impactlab_website/job_bcsd_ir_slurm_labor.py
@@ -19,26 +19,26 @@ __author__ = 'Justin Gerard'
 __contact__ = 'jsimcock@rhg.com'
 __version__ = '0.1.0'
 
-# BCSD_orig_files  = (
-#     '/global/scratch/jiacany/nasa_bcsd/raw_data/{rcp}/' +
-#     '{model}/{variable}/' +
-#     '{variable}_day_BCSD_{rcp}_r1i1p1_{model}_{{year}}.nc')
+BCSD_orig_files  = (
+    '/global/scratch/jiacany/nasa_bcsd/raw_data/{rcp}/' +
+    '{model}/{variable}/' +
+    '{variable}_day_BCSD_{rcp}_r1i1p1_{model}_{{year}}.nc')
 
 ##sacag test
 
-BCSD_orig_files = os.path.join(
-    '/shares/gcp/sources/BCSD-original/{rcp}/day/atmos/{variable}/r1i1p1/v1.0',
-    '{variable}_day_BCSD_{rcp}_r1i1p1_{model}_{{year}}.nc')
-
-WRITE_PATH = os.path.join(
-    '~/data/{agglev}/{rcp}',
-    '{variable}/{transformation_name}/{model}',
-    '{variable}_{transformation_name}_{model}_{{year}}.nc')
+# BCSD_orig_files = os.path.join(
+#     '/shares/gcp/sources/BCSD-original/{rcp}/day/atmos/{variable}/r1i1p1/v1.0',
+#     '{variable}_day_BCSD_{rcp}_r1i1p1_{model}_{{year}}.nc')
 
 # WRITE_PATH = os.path.join(
-#     '/global/scratch/jsimcock/gcp/climate/{agglev}/{rcp}',
+#     '~/data/{agglev}/{rcp}',
 #     '{variable}/{transformation_name}/{model}',
 #     '{variable}_{transformation_name}_{model}_{{year}}.nc')
+
+WRITE_PATH = os.path.join(
+    '/global/scratch/jsimcock/gcp/climate/{agglev}/{rcp}',
+    '{variable}/{transformation_name}/{model}',
+    '{variable}_{transformation_name}_{model}_{{year}}.nc')
 
 
 
@@ -65,31 +65,31 @@ JOBS = [
 
     ]
 
-PERIODS = [dict(rcp='historical' , pername='annual', years=list(range(1981, 1986)))]
-        # dict(rcp='rcp85', pername='annual', years=list(range(2006, 2100))))
+PERIODS = [dict(rcp='historical' , pername='annual', years=list(range(1981, 2006))),
+            dict(rcp='rcp85', pername='annual', years=list(range(2006, 2100)))]
 
 MODELS = list(map(lambda x: dict(model=x), [
-    'ACCESS1-0']))
-    # 'bcc-csm1-1',
-    # 'BNU-ESM',
-    # 'CanESM2',
-    # 'CCSM4',
-    # 'CESM1-BGC',
-    # 'CNRM-CM5',
-    # 'CSIRO-Mk3-6-0',
-    # 'GFDL-CM3',
-    # 'GFDL-ESM2G',
-    # 'GFDL-ESM2M',
-    # 'IPSL-CM5A-LR',
-    # 'IPSL-CM5A-MR',
-    # 'MIROC-ESM-CHEM',
-    # 'MIROC-ESM',
-    # 'MIROC5',
-    # 'MPI-ESM-LR',
-    # 'MPI-ESM-MR',
-    # 'MRI-CGCM3',
-    # 'inmcm4',
-    # 'NorESM1-M']))
+    'ACCESS1-0',
+    'bcc-csm1-1',
+    'BNU-ESM',
+    'CanESM2',
+    'CCSM4',
+    'CESM1-BGC',
+    'CNRM-CM5',
+    'CSIRO-Mk3-6-0',
+    'GFDL-CM3',
+    'GFDL-ESM2G',
+    'GFDL-ESM2M',
+    'IPSL-CM5A-LR',
+    'IPSL-CM5A-MR',
+    'MIROC-ESM-CHEM',
+    'MIROC-ESM',
+    'MIROC5',
+    'MPI-ESM-LR',
+    'MPI-ESM-MR',
+    'MRI-CGCM3',
+    'inmcm4',
+    'NorESM1-M']))
 
 AGGREGATIONS = [{'agglev': 'hierid', 'aggwt': 'popwt'}]
 
